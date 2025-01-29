6fb122a5deb9ebc60c53b4d992992ae39a0f5597
RubenOdd <rubenodd1002@gmail.com>
2025-01-29 14:13:31 +0200

    moving your character

--- Scripts/ProtoMover.cs
+++ Scripts/ProtoMover.cs
@@ -6,24 +6,17 @@ namespace SnekGame
     public partial class ProtoMover : CharacterBody2D
     {
         [Export] private float _speed = 100.0f;
-        [Export(PropertyHint.Enum, "0,1")] private int _direction;
 
-        public override void _Ready()
+        public void GetInput()
         {
-            GlobalPosition = new Vector2(0,0);
+            Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+            Velocity = inputDirection * _speed;
         }
 
-        public override void _Process(double delta)
+        public override void _PhysicsProcess(double delta)
         {
-            if (_direction == 0) 
-            {
-                GlobalPosition += Vector2.Right * (float)delta * _speed;
-            }
-            else if (_direction == 1)
-            {
-                GlobalPosition += Vector2.Down * (float)delta * _speed;
-            }
+            GetInput();
+            MoveAndCollide(Velocity * (float)delta);
         }
-
     }
 }
